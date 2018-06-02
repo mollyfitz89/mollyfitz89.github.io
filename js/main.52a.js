a4c85a05470e6deb566076a21ab5044ee0906246
Sam Lau <samuel.kwun.lau@gmail.com>
2018-06-02 00:13:52 -0400

    redesign

--- js/main.52a.js
+++ js/main.52a.js
@@ -26,10 +26,12 @@
         return groupedClips;
       }, {});
 
-    var publishers = Object.keys(clipsByPublisher);
-    var accordionEl = document.querySelectorAll('#accordion')[0];
-
-    renderAccordion($(accordionEl), publishers, clipsByPublisher, bindEventHandlers);
+    renderAccordion(
+      $('#accordion'),
+      Object.keys(clipsByPublisher),
+      clipsByPublisher,
+      afterRender
+    );
   });
 
   function renderAccordion($rootEl, publishers, clipsByPublisher, cb) {
@@ -37,17 +39,7 @@
       return $(makePanel(publisher, clipsByPublisher[publisher]));
     });
 
-    var appendInterval = setInterval(function() {
-      if (panels.length) {
-        var $panel = panels.splice(0, 1)[0];
-
-        $rootEl.append($panel);
-        $panel.fadeIn()
-      } else {
-        clearInterval(appendInterval);
-      }
-    }, 125);
-
+    $rootEl.append(panels);
     cb();
   }
 
@@ -56,10 +48,15 @@
       return strOfAnchors + '<li><div><a target="_blank" href="' + clip.link + '">' + clip.headline + '</a></div></li>';
     }, '');
 
-    return '<div class="accordion__container"><div class="accordion__menu"><h3>' + title + '</h3></div><ul class="accordion__items">' + anchoredClips + '</ul></div>';
+    return '<div class="accordion__container"><div class="accordion__menu"><h3 class="accordion__menu-publication">' + title + '</h3></div><ul class="accordion__items">' + anchoredClips + '</ul></div>';
   }
 
-  function bindEventHandlers() {
+  function afterRender() {
+    $('.loader').fadeOut(500);
+    setTimeout(function() {
+      $('.main').fadeIn(750);
+    }, 500);
+    // bind event handlers
     $('#accordion').on('click', '.accordion__menu', function() {
       $(this).parent().find('.accordion__items').slideToggle();
     });
